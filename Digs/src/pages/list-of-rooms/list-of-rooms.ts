d41c7bcbe4430fd563c356c04c820f5d13005f6c
andryuha77 <andryuha1977@gmail.com>
2018-02-28 21:14:20 +0000

    List of images moved to list-of-rooms page

--- Digs/src/pages/list-of-rooms/list-of-rooms.ts
+++ Digs/src/pages/list-of-rooms/list-of-rooms.ts
@@ -1,5 +1,8 @@
+import { HomePage } from './../home/home';
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ModalController, ActionSheetController } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
+import { ImagesProvider } from './../../providers/images/images';
 
 /**
  * Generated class for the ListOfRoomsPage page.
@@ -14,12 +17,94 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'list-of-rooms.html',
 })
 export class ListOfRoomsPage {
+  images: any = [];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  private UID: String;
+  private AdID: String;
+  private RoomType: String;
+  private College: String;
+  private Eircode: String;
+  private LocationDes: String;
+  private Price: any;
+  private Availability: any;
+  private Email: String;
+
+  public newAd: any;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private imagesProvider: ImagesProvider, private camera: Camera, private actionSheetCtrl: ActionSheetController, private modalCtrl: ModalController) {
+    this.reloadImages();
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ListOfRoomsPage');
+  uploadImages() {
+    
   }
 
+  publishAd() {
+    this.navCtrl.setRoot(HomePage);
+  }
+  reloadImages() {
+    this.imagesProvider.getImages().subscribe(data => {
+      this.images = data;
+    });
+  }
+ 
+  deleteImage(img) {
+    this.imagesProvider.deleteImage(img).subscribe(data => {
+      this.reloadImages();
+    });
+  }
+ 
+  openImage(img) {
+    let modal = this.modalCtrl.create('PreviewModalPage', { img: img });
+    modal.present();
+  }
+ 
+  presentActionSheet() {
+    let actionSheet = this.actionSheetCtrl.create({
+      title: 'Select Image Source',
+      buttons: [
+        {
+          text: 'Load from Library',
+          handler: () => {
+            this.takePicture(this.camera.PictureSourceType.PHOTOLIBRARY);
+          }
+        },
+        {
+          text: 'Use Camera',
+          handler: () => {
+            this.takePicture(this.camera.PictureSourceType.CAMERA);
+          }
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel'
+        }
+      ]
+    });
+    actionSheet.present();
+  }
+ 
+  public takePicture(sourceType) {
+    // Create options for the Camera Dialog
+    var options = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      sourceType: sourceType,
+      saveToPhotoAlbum: false,
+      correctOrientation: true
+    };
+ 
+    // Get the data of an image
+    this.camera.getPicture(options).then((imagePath) => {
+      let modal = this.modalCtrl.create('UploadModalPage', { data: imagePath });
+      modal.present();
+      modal.onDidDismiss(data => {
+        if (data && data.reload) {
+          this.reloadImages();
+        }
+      });
+    }, (err) => {
+      console.log('Error: ', err);
+    });
+  }
 }
