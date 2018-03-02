7f14c87e2b300ba82f6891b62c9dc585c532023f
andryuha77 <andryuha1977@gmail.com>
2018-03-02 23:30:24 +0000

    adding more parameters to the front-end

--- Digs/src/providers/images/images.ts
+++ Digs/src/providers/images/images.ts
@@ -6,40 +6,43 @@ import { FileTransfer, FileUploadOptions, FileTransferObject } from '@ionic-nati
 
 @Injectable()
 export class ImagesProvider {
-   //apiURL = 'http://localhost:3000/';
-   //  apiURL = 'http://54.187.101.201:3000/'; //patrick
-    apiURL = 'http://52.56.193.204:3000/'; // andrei
-   
- 
+  //apiURL = 'http://localhost:3000/';
+  //  apiURL = 'http://54.187.101.201:3000/'; //patrick
+  apiURL = 'http://52.56.193.204:3000/'; // andrei
+
+
   constructor(public http: Http, private transfer: FileTransfer) { }
- 
+
   getImages() {
     return this.http.get(this.apiURL + 'images').map(res => res.json());
   }
- 
+
   deleteImage(img) {
     return this.http.delete(this.apiURL + 'images/' + img._id);
   }
- 
-  uploadImage(img, desc) {
- 
+
+  uploadImage(img, desc, roomType, college, eircode, location, price, availabilty, email, phone, howContact, parking) {
+
     // Destination URL
     let url = this.apiURL + 'images';
- 
+
     // File for Upload
     var targetPath = img;
- 
+
     var options: FileUploadOptions = {
       fileKey: 'image',
       chunkedMode: false,
       mimeType: 'multipart/form-data',
-      params: { 'desc': desc }
+      params: {
+        'desc': desc, 'roomType': roomType, 'college': college, 'eircode': eircode, 'location': location,
+        'price': price, 'availabilty': availabilty, 'email': email, 'phone': phone, 'howContact': howContact, 'parking': parking
+      }
     };
- 
+
     const fileTransfer: FileTransferObject = this.transfer.create();
- 
+
     // Use the FileTransfer to upload the image
     return fileTransfer.upload(targetPath, url, options);
   }
- 
+
 }
