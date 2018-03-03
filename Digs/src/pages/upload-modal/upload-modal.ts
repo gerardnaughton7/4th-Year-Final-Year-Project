747c0d83f3edecc1e52d37f700b88bab5629739d
andryuha77 <andryuha1977@gmail.com>
2018-03-03 13:57:26 +0000

    added parameters to upload-modal.ts

--- Digs/src/pages/upload-modal/upload-modal.ts
+++ Digs/src/pages/upload-modal/upload-modal.ts
@@ -17,13 +17,24 @@ import { IonicPage, NavController, NavParams, ViewController  } from 'ionic-angu
 export class UploadModalPage {
   imageData: any;
   desc: string;
- 
+  roomType: string;
+  college: string; 
+  eircode: string
+  location: string 
+  price: string 
+  availabilty: string
+  email: string
+  phone: string
+  howContact: string
+  parking: string
+
   constructor(public navCtrl: NavController, private navParams: NavParams, private viewCtrl: ViewController, private imagesProvider: ImagesProvider) {
     this.imageData = this.navParams.get('data');
   }
  
   saveImage() {
-    this.imagesProvider.uploadImage(this.imageData, this.desc).then(res => {
+    this.imagesProvider.uploadImage(this.imageData, this.desc,
+      this.roomType,  this.college,  this.eircode,  this.location,  this.price,  this.availabilty,  this.email,  this.phone,  this.howContact,  this.parking).then(res => {
       this.viewCtrl.dismiss({reload: true});
     }, err => {
       this.dismiss();
