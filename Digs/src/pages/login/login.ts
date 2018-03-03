5997cb86cd256b9ae39c1550dcad94a1e838c60b
moranpatrick <patrickmoran121@gmail.com>
2018-03-03 17:37:19 +0000

    Started Coding Google Login Functionality using @ionic-native/google-plus

--- Digs/src/pages/login/login.ts
+++ Digs/src/pages/login/login.ts
@@ -5,6 +5,9 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { AngularFireAuth } from 'angularfire2/auth';
 
+import { GooglePlus } from '@ionic-native/google-plus';
+import firebase from 'firebase';
+
 @IonicPage()
 @Component({
   selector: 'page-login',
@@ -15,7 +18,8 @@ export class LoginPage {
   //Initialize a new User Object Here
   user = {} as User
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private afAuth: AngularFireAuth) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, 
+    private afAuth: AngularFireAuth, public googlePlus: GooglePlus) {
   }
 
   ionViewDidLoad() {
@@ -37,6 +41,22 @@ export class LoginPage {
 
   register(){
     this.navCtrl.push(RegisterPage);
+  }
+
+  googleLogin(){
+    this.googlePlus.login({
+      'webClientId': '899080047110-r464tup6omrqfci8lce54nhtlm8j4gp0.apps.googleusercontent.com',
+      'offline': true
+    }).then(res => {
+      // Store the Id Token
+      firebase.auth().signInWithCredential(firebase.auth.GoogleAuthProvider.credential(res.idToken)).then(suc => {
+        this.navCtrl.setRoot(HomePage);
+        alert("Google Login Success!");
+
+      }).catch(err => {
+        alert("Google Login Failed!");
+      })
+    });
   }
 
 }
