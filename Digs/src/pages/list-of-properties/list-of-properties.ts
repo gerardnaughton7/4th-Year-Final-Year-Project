68a0870ad5e149784dd9a4babd2005275a884242
moranpatrick <patrickmoran121@gmail.com>
2018-03-13 23:29:54 +0000

    Made some small changes to the slider that displays the ad images - images are displayed cleaner, added pager and looping functionality

--- Digs/src/pages/list-of-properties/list-of-properties.ts
+++ Digs/src/pages/list-of-properties/list-of-properties.ts
@@ -2,13 +2,6 @@ import { PropertyAd } from './../../providers/propertyAd';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, ModalController } from 'ionic-angular';
 
-/**
- * Generated class for the ListOfPropertiesPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-list-of-properties',
