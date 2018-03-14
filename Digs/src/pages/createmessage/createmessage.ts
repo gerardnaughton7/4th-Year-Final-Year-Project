e443041429b8cccc0bf30498c77283d8ecdda4dc
andryuha77 <andryuha1977@gmail.com>
2018-03-14 22:25:41 +0000

    Front end for create message page, geting error: No provider for Message!

--- Digs/src/pages/createmessage/createmessage.ts
+++ Digs/src/pages/createmessage/createmessage.ts
@@ -1,5 +1,9 @@
+import { ImagesProvider } from './../../providers/images/images';
+import { Md5 } from 'ts-md5/dist/md5';
+import { Message } from './../../providers/message';
+import { HomePage } from './../home/home';
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, ViewController, NavParams} from 'ionic-angular';
 
 /**
  * Generated class for the CreatemessagePage page.
@@ -14,12 +18,37 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'createmessage.html',
 })
 export class CreatemessagePage {
+  time: any = new String(new Date());
+  UID: String;
+  AdID: any = Md5.hashStr(this.time);
+  Message: String;
+  Name: String;
+  Email: String;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public messageService: Message, public navParams: NavParams,
+    public viewCtrl: ViewController, private imagesProvider: ImagesProvider) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad CreatemessagePage');
   }
 
+  postMessage() {
+
+    this.imagesProvider.getImageAdID(this.AdID).map(res => res.json()).subscribe(data => {
+      alert('My Data: ' + data + " And Data is a: " + typeof(data));
+
+    let message = {
+      UID: this.UID,
+      AdID: this.AdID,
+      Name: this.Name,
+      Email: this.Email
+    };
+    this.messageService.createMessage(message);  
+    this.navCtrl.setRoot(HomePage);
+  });
+  }
+
 }
+
+
---
+++ Digs/src/providers/message.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import 'rxjs/add/operator/map';
+ 
+@Injectable()
+export class Message {
+ 
+  data: any;
+  //apiURL = 'http://localhost:8080/';
+  apiURL = 'http://54.73.1.214:8080/'; //patrick
+  //apiURL = 'http://52.56.193.204:8080/'; // andrei
+  //apiURL = 'http://54.72.69.79:8080/'; //gerard
+ 
+  constructor(public http: Http) {
+    this.data = null;
+  }
+ 
+  getMessage(){
+ 
+    if (this.data) {
+      return Promise.resolve(this.data);
+    }
+ 
+    return new Promise(resolve => {
+ 
+      this.http.get(this.apiURL+'api/message')
+        .map(res => res.json())
+        .subscribe(data => {
+          this.data = data;
+          resolve(this.data);
+        });
+    });
+ 
+  }
+ 
+  createMessage(message){
+ 
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    this.http.post(this.apiURL+'api/message', JSON.stringify(message), {headers: headers})
+      .subscribe(res => {
+        console.log(res.json());
+      },
+      error => {
+        alert("ERROR CREATING Message: " + error);
+      });
+ 
+  }
+ 
+  deleteMessage(id){
+ 
+    this.http.delete(this.apiURL+'message/' + id).subscribe((res) => {
+      console.log(res.json());
+    });   
+ 
+  }
+ 
+}
